9ac60e525124f7c3be9083837fc0578418971479
iminurnamez <easinerf@gmail.com>
2015-07-24 15:11:40 -0400

    Challenge

--- actors.py
+++ actors.py
@@ -1,3 +1,4 @@
+from math import sin, cos, radians
 import random
 import itertools
 import pygame as pg
@@ -6,12 +7,20 @@ import prepare
 import tools
 
 
+def footprint_collide(left, right):
+    """
+    Checks for collision between two sprites using their footprint rects instead of
+    their image rects. Used as callback for sprite collision detection methods.
+    """
+    return left.footprint.colliderect(right.footprint)
+
+  
 SPRITE_SIZE = (32, 36)
 
 
 class RPGSprite(pg.sprite.DirtySprite):
     """Base class for player and AI sprites."""
-    def __init__(self, pos, speed, name, facing="DOWN", *groups):
+    def __init__(self, pos, speed,  footprint_size, name, facing="DOWN", *groups):
         super(RPGSprite, self).__init__(*groups)
         self.speed = speed
         self.name = name
@@ -24,7 +33,10 @@ class RPGSprite(pg.sprite.DirtySprite):
         self.walkframes = None
         self.walkframe_dict = self.make_frame_dict(self.get_frames(name))
         self.adjust_images()
-        self.rect = self.image.get_rect(center=pos)
+        self.rect = self.image.get_rect(midbottom=pos)
+        #rect for collision detection
+        self.footprint = pg.Rect((0,0), footprint_size)
+        self.footprint.midbottom = self.rect.midbottom
         self.dirty = 1
 
     def get_frames(self, character):
@@ -40,6 +52,55 @@ class RPGSprite(pg.sprite.DirtySprite):
             frame_dict[direct] = itertools.cycle([frames[i][0], frames[i][2]])
         return frame_dict
 
+    def wrap_move(self, screen_rect):
+        """
+        Move sprite's rect and footprint to opposite side of
+        screen if rect is completely off-screen.
+        """
+        if self.rect.right < 0:
+            self.rect.move_ip((screen_rect.width, 0))
+            self.dirty = 1
+        elif self.rect.left > screen_rect.right:
+            self.rect.move_ip((-screen_rect.width, 0))
+            self.dirty = 1
+        elif self.rect.bottom < 0:
+            self.rect.move_ip((0, screen_rect.height))
+            self.dirty = 1
+        elif self.rect.top > screen_rect.bottom:
+            self.rect.move_ip((0, -screen_rect.height))
+            self.dirty = 1
+        self.footprint.midbottom = self.rect.midbottom
+        
+    def screen_wrap(self, screen_rect, all_sprites, wrapped_sprites):
+        """
+        Create a temporary sprite to display the portion of self.image
+        that is off-screen and adds it to all_sprites for drawing and
+        wrapped_sprites for culling on the next tick.
+        """        
+        x, y = self.rect.topleft
+        w, h = self.rect.size
+        if self.rect.left < 0:
+            sub_rect = pg.Rect((0, 0), (abs(x), h))
+            wrap_rect = pg.Rect((screen_rect.right + x, y), sub_rect.size)
+        elif self.rect.right >  screen_rect.right:
+            sub_w = self.rect.right - screen_rect.right
+            sub_rect = pg.Rect((w - sub_w, 0), (sub_w, h))
+            wrap_rect = pg.Rect((0, y), sub_rect.size)
+        elif self.rect.top < 0:
+            sub_rect = pg.Rect((0, 0), (w, abs(y)))
+            wrap_rect = pg.Rect((x, screen_rect.bottom + y), sub_rect.size)
+        elif self.rect.bottom > screen_rect.bottom:
+            sub_h = self.rect.bottom - screen_rect.bottom
+            sub_rect = pg.Rect((0, h - sub_h), (w, sub_h))
+            wrap_rect = pg.Rect((x, 0), sub_rect.size)
+        
+        sprite = pg.sprite.DirtySprite(all_sprites, wrapped_sprites)
+        sprite.image = self.image.subsurface(sub_rect)
+        sprite.rect = wrap_rect
+        sprite.dirty = 1
+        all_sprites.change_layer(sprite, sprite.rect.bottom)
+        self.dirty = 1
+            
     def adjust_images(self, now=0):
         """Update the sprite's walkframes as the sprite's direction changes."""
         if self.direction != self.old_direction:
@@ -75,25 +136,43 @@ class RPGSprite(pg.sprite.DirtySprite):
             self.direction_stack.remove(direction)
         if self.direction_stack:
             self.direction = self.direction_stack[-1]
-
-    def update(self, now, screen_rect):
+    
+    def bounce(self, rect, direction, bounce_amount=4):
+        """
+        Bounce sprite off an obstacle by bounce amount in the opposite
+        direction the sprite is travelling. 
+        """
+        offsets = {"LEFT": (rect.right - (self.footprint.left - bounce_amount), 0),
+                    "RIGHT": (rect.left - (self.footprint.right + bounce_amount), 0),
+                    "UP": (0, rect.bottom - (self.footprint.top - bounce_amount)),
+                    "DOWN": (0, rect.top - (self.footprint.bottom + bounce_amount))}
+        self.rect.move_ip(offsets[direction])
+        self.footprint.midbottom = self.rect.midbottom
+        
+    def update(self, now, screen_rect, all_sprites, wrapped_sprites):
         """Update image and position of sprite."""
         self.adjust_images(now)
+        self.wrap_move(screen_rect)
+        if self.rect.clamp(screen_rect) != self.rect:
+            self.screen_wrap(screen_rect, all_sprites, wrapped_sprites)
+            self.dirty = 1
+        
         if self.direction_stack:
             direction_vector = prepare.DIRECT_DICT[self.direction]
             self.rect.x += self.speed*direction_vector[0]
             self.rect.y += self.speed*direction_vector[1]
             self.dirty = 1
-
+        self.footprint.midbottom = self.rect.midbottom
+        
     def draw(self, surface):
         """Draw sprite to surface (not used if using group draw functions)."""
-        surface.blit(self.image, self.rect)
-        
+        return surface.blit(self.image, self.rect)
+       
 
 class Player(RPGSprite):
     """This class will represent the user controlled character."""
-    def __init__(self, pos, speed, name="warrior_m", facing="DOWN", *groups):
-        super(Player, self).__init__(pos, speed, name, facing, *groups)
+    def __init__(self, pos, speed, footprint_size, name="warrior_m",  facing="DOWN", *groups):
+        super(Player, self).__init__(pos, speed, footprint_size, name, facing, *groups)
 
     def get_event(self, event):
         """Handle events pertaining to player control."""
@@ -102,10 +181,17 @@ class Player(RPGSprite):
         elif event.type == pg.KEYUP:
             self.pop_direction(event.key)
 
-    def update(self, now, screen_rect):
+    def update(self, now, screen_rect, all_sprites, wrapped_sprites):
         """Call base classes update method and clamp player to screen."""
-        super(Player, self).update(now, screen_rect)
-        self.rect.clamp_ip(screen_rect)
+        super(Player, self).update(now, screen_rect, all_sprites, wrapped_sprites)
+    
+    def collide_with_walls(self, walls):
+        """
+        Bounce off the first wall in walls (a list of allwalls that the sprite collides with).
+        Only responding to one collision per tick avoids rattling around in corners or
+        "bouncing" multiple times.
+        """        
+        self.bounce(walls[0].footprint, self.direction)
 
     def add_direction(self, key):
         """Remove direction from stack if corresponding key is released."""
@@ -120,40 +206,54 @@ class Player(RPGSprite):
 
 class AISprite(RPGSprite):
     """A non-player controlled sprite."""
-    def __init__(self, pos, speed, name, facing, *groups):
-        super(AISprite, self).__init__(pos, speed, name, facing, *groups)
+    def __init__(self, pos, speed, footprint_size, name, facing, *groups):
+        super(AISprite, self).__init__(pos, speed, footprint_size, name, facing, *groups)
         self.wait_range = (500, 2000)
         self.wait_delay = random.randint(*self.wait_range)
         self.wait_time = 0.0
         self.change_direction()
 
-    def update(self, now, screen_rect):
+    def update(self, now, screen_rect, all_sprites, wrapped_sprites):
         """
         Choose a new direction if wait_time has expired or the sprite
         attempts to leave the screen.
         """
         if now-self.wait_time > self.wait_delay:
             self.change_direction(now)
-        super(AISprite, self).update(now, screen_rect)
-        if not screen_rect.contains(self.rect):
-            self.change_direction(now)
-            self.rect.clamp_ip(screen_rect)
-
-    def change_direction(self, now=0):
+        super(AISprite, self).update(now, screen_rect, all_sprites, wrapped_sprites)
+        
+    def collide_with_walls(self, walls):
+        """
+        Bounce off the first wall in walls (a list of allwalls that the sprite collides with)
+        and change direction to something other than the sprite's current direction.
+        """
+        self.bounce(walls[0].footprint, self.direction)
+        self.change_direction(restricted=self.direction)
+        
+    def change_direction(self, now=0, restricted=None):
         """
         Empty the stack and choose a new direction.  The sprite may also
-        choose not to go idle (choosing direction=None)
+        choose not to go idle (choosing direction=None). Passing a direction
+        as restricted eliminates that direction and None from the possible
+        new directions.
         """
         self.direction_stack = []
-        direction = random.choice(prepare.DIRECTIONS+(None,))
+        directions = list(prepare.DIRECTIONS + (None, ))
+        if restricted:
+            directions.remove(restricted)
+            directions.remove(None)
+        direction = random.choice(directions)
         if direction:
             super(AISprite, self).add_direction(direction)
+        
         self.wait_delay = random.randint(*self.wait_range)
         self.wait_time = now
 
 
 class Obstacle(pg.sprite.DirtySprite):
-    def __init__(self, pos, *groups):
+    def __init__(self, pos, footprint_size, *groups):
         super(Obstacle, self).__init__(*groups)
         self.image = prepare.GFX["stone"]
         self.rect = self.image.get_rect(topleft=pos)
+        self.footprint = pg.Rect((0,0), footprint_size)
+        self.footprint.midbottom = self.rect.midbottom
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python 
 import sys
 import random
+import itertools
 import pygame as pg
 
 # Importing prepare initializes the display.
@@ -9,9 +10,10 @@ import prepare
 import actors
         
 
-MAX_NPCS = 7
+MAX_NPCS = 20
+DEBUG = False
 
-            
+
 class App(object):
     """This is the main class that runs the program."""
     def __init__(self):
@@ -22,26 +24,58 @@ class App(object):
         self.fps = 60
         self.done = False
         self.all_sprites = pg.sprite.LayeredDirty()
-        self.player = actors.Player(self.screen_rect.center, 3)
+        #portions of npcs and player that wrap screen, all are Sprite.killed each tick
+        self.wrapped_sprites = pg.sprite.Group()
+        #holds NPCs + player
+        self.people = pg.sprite.Group()
+        self.player = actors.Player(self.screen_rect.center, 3, (30, 6))
         self.obstacles = self.make_obstacles()
         self.npcs = self.make_npcs()
+        self.people.add(self.player)
         self.all_sprites.add(self.player)
-        self.all_sprites.clear(self.screen, self.bg)         
-        
-    def make_npcs(self):
-        """Create a group of NPCs and add them to the all_sprites group."""
+        self.all_sprites.clear(self.screen, self.bg)
+                    
+    def divide_screen(self, footprint_height):
+        """
+        Divides the screen into (TILE_SIZE, footprint_height)-sized rects.
+        Returns a list of the midbottom positions of the rects that don't
+        collide with any obstacles.
+        """
         size = prepare.TILE_SIZE
         width, height = prepare.SCREEN_SIZE
+        fp_height = footprint_height
+        spots = [pg.Rect((x, y), (size, footprint_height))
+                    for x in range(0, width + 1, size)
+                    for y in range(0, height + 1, footprint_height)
+                ]
+        open_spots = pg.sprite.Group()
+        #Turn spots into Sprites 
+        for spot in spots:
+            sprite = pg.sprite.Sprite(open_spots)
+            sprite.footprint = spot
+        taken = self.obstacles.copy()
+        taken.add(self.player)
+        pg.sprite.groupcollide(taken, open_spots, False, True,
+                                         actors.footprint_collide)
+        return [open_spot.footprint.midbottom for open_spot in open_spots]
+        
+    def make_npcs(self):
+        """
+        Create a group of NPCs and add them to the all_sprites group.
+        """
+        #Find all open positions and grab one for each NPC
+
+        open_positions = random.sample(self.divide_screen(6), MAX_NPCS)  
         characters = list(prepare.GFX["characters"].keys())
         npcs = pg.sprite.Group()
-        while len(npcs) < MAX_NPCS:
+        for pos in open_positions:
             name = random.choice(characters)
-            if name != self.player.name:
-                pos = [random.randint(size, width-size*2), 
-                       random.randint(size, height-size*2)]
-                speed = random.randint(1,2)
-                way = random.choice(prepare.DIRECTIONS)
-                actors.AISprite(pos, speed, name, way, self.all_sprites, npcs)
+            while name == self.player.name:
+                name = random.choice(characters)
+            speed = random.randint(1,3)
+            way = random.choice(prepare.DIRECTIONS)
+            actors.AISprite(pos, speed, (30, 6), name, way, self.all_sprites,
+                                   npcs, self.people)    
         return npcs
 
     def make_obstacles(self):
@@ -51,24 +85,25 @@ class App(object):
         size = prepare.TILE_SIZE
         width, height = prepare.SCREEN_SIZE
         walls = pg.sprite.Group()
+        footprint_size = (28, 30)
         for i in range(0, width, size):
             if i < width//2-size*2 or i >= width//2+size*2:
-                actors.Obstacle((i,-size), walls)
-                actors.Obstacle((i,0), self.all_sprites, walls)
-                actors.Obstacle((i,height-size), self.all_sprites, walls)
-                actors.Obstacle((i,height), walls)
+                actors.Obstacle((i,-size), footprint_size, walls)
+                actors.Obstacle((i,0), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((i,height-size), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((i,height), footprint_size, walls)
             if size*4 <= i < size*7 or width-size*4 > i >= width-size*7:
-                actors.Obstacle((i,size*4), self.all_sprites, walls)
-                actors.Obstacle((i,height-size*5), self.all_sprites, walls)
+                actors.Obstacle((i,size*4), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((i,height-size*5), footprint_size, self.all_sprites, walls)
         for j in range(size, height-size, size):
             if j < height//2-size*2 or j >= height//2+size*2:
-                actors.Obstacle((-size,j), walls)
-                actors.Obstacle((0,j), self.all_sprites, walls)
-                actors.Obstacle((width-size,j), self.all_sprites, walls)
-                actors.Obstacle((width,j), walls)
+                actors.Obstacle((-size,j), footprint_size, walls)
+                actors.Obstacle((0,j), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((width-size,j), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((width,j), footprint_size, walls)
             if size*5 <= j < size*7 or height-size*5 > j >= height-size*7:
-                actors.Obstacle((size*6,j), self.all_sprites, walls)
-                actors.Obstacle((width-size*7,j), self.all_sprites, walls)
+                actors.Obstacle((size*6,j), footprint_size, self.all_sprites, walls)
+                actors.Obstacle((width-size*7,j), footprint_size, self.all_sprites, walls)
         return walls
 
     def event_loop(self):
@@ -90,12 +125,24 @@ class App(object):
     def update(self):
         """Update all actors."""
         now = pg.time.get_ticks()
-        self.all_sprites.update(now, self.screen_rect)
+        for wrapped in self.wrapped_sprites:
+            wrapped.kill()
+        
+        self.all_sprites.update(now, self.screen_rect, self.all_sprites,
+                                         self.wrapped_sprites)
+        
+        #Get all collisions between people and obstacles using their
+        #footprints for collison detection.  
+        wall_collisions = pg.sprite.groupcollide(self.people, self.obstacles,
+                                                                 False, False, actors.footprint_collide)
+        #Deal with the collisions.                                                         
+        for person, walls in wall_collisions.items():
+            person.collide_with_walls(walls)        
         for sprite in self.all_sprites:
             layer = self.all_sprites.get_layer_of_sprite(sprite)
             if layer != sprite.rect.bottom:
                 self.all_sprites.change_layer(sprite, sprite.rect.bottom)
-
+                
     def render(self):
         """
         Render all actors.
